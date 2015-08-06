6151aaad289281acc9f6508daed5d37b062047ce
Silvia Brown <siltastic@gmail.com>
2015-08-06 14:03:11 -0400

    Added resources for users and connected static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  # Devise
   devise_for :users
 
   # Trigger the index action in our posts controller.
   root to: 'prints#index'
-  
+
+  # Get the static pages.
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
+
   # Generate RESTful routes for the models.
+  resources :users
   resources :prints do
     resources :comments
   end
