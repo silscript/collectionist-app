6bb956de29a21a176331f476debc6fdd127922c1
Silvia Brown <siltastic@gmail.com>
2015-08-06 01:22:00 -0400

    Added the create method and strong params.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+
+  # Create the create route.
+  def create
+    @print = Print.find(params[:print_id])
+    @comment = @print.comments.create(comment_params)
+
+    # Redirect it to the show page of the print.
+    redirect_to print_path(@print)
+  end
+
+  # # Create the destroy route.
+  # def destroy
+  #   @print = Print.find(params[:print_id])
+  #   @comment = @print.comments.find(params[:id])
+  #   @comment.destroy
+    
+  #   # Redirect to the show page of the print.
+  #   redirect_to post_path(@print)
+  # end
+
+  # Add strong params for privacy.
+  private
+
+  def comment_params
+    params.require(:comment).permit(:username, :content)
+  end
+
 end
