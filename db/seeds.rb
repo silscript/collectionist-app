1eea96dd627f0e74f8ede8c8641b09d598f88aac
Silvia Brown <siltastic@gmail.com>
2015-08-06 01:15:09 -0400

    Added print data for the first two user ids.

---
+++ db/seeds.rb
@@ -0,0 +1,261 @@
+# User One
+print_one = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Andrew Kolb", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Home-Sweet Jellystone",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_AndrewKolb-YogiBear.jpg",
+                          series: "Yogi Bear",
+                          purchase_year: "2014",
+                          purchase_price: "$45.00",
+                          user_id: 1
+                        )
+
+print_two = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Anne Benjamin", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The Two Mousketeers",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_AnneBenjamin-TomAndJerry.jpg",
+                          series: "Tom & Jerry",
+                          purchase_year: "2014",
+                          purchase_price: "$45.00",
+                          user_id: 1
+                        )
+
+print_three = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Kilian Eng", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Batman Beyond",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_KilianEng-BatmanBeyond.jpg",
+                          series: "Batman",
+                          purchase_year: "2014",
+                          purchase_price: "$45.00",
+                          user_id: 1
+                        )
+
+print_four = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Aladdin",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-Aladdin.jpeg",
+                          series: "Aladdin",
+                          purchase_year: "2014",
+                          purchase_price: "$45.00",
+                          user_id: 1
+                        )
+
+print_five = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The Incredibles",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-TheIncredibles.jpeg",
+                          series: "The Incredibles",
+                          purchase_year: "2014",
+                          purchase_price: "$50.00",
+                          user_id: 1
+                        )
+
+print_six = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Dave Perillo", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Winnie the Pooh",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-WinnieThePooh.jpeg",
+                          series: "Winnie the Pooh",
+                          purchase_year: "2014",
+                          purchase_price: "$45.00",
+                          user_id: 1
+                        )
+
+print_seven = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Coraline",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-Coraline.jpeg",
+                          series: "Coraline",
+                          purchase_year: "2014",
+                          purchase_price: "$50.00",
+                          user_id: 1
+                        )
+
+print_eight = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Frozen",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-Frozen.jpeg",
+                          series: "Frozen",
+                          purchase_year: "2014",
+                          purchase_price: "$60.00",
+                          user_id: 1
+                        )
+
+print_nine = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "DKNG", 
+                          creator_company: "Mondo",
+                          creator_printer: ""
+                          title: "The Iron Giant",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_DKNG-TheIronGiant.jpg",
+                          series: "The Iron Giant",
+                          purchase_year: "2014",
+                          purchase_price: "$75.00",
+                          user_id: 1
+                        )
+
+print_ten = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Teenage Mutant Ninja Turtles",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-TMNT.jpg",
+                          series: "Teenage Mutant Ninja Turtles",
+                          purchase_year: "2014",
+                          purchase_price: "$70.00",
+                          user_id: 1
+                        )
+
+print_eleven = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The Lego Movie",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-TheLegoMovie.jpeg",
+                          series: "The Lego Movie",
+                          purchase_year: "2014",
+                          purchase_price: "$70.00",
+                          user_id: 1
+                        )
+
+print_twelve = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tom Whalen", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Mad Monster Party",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TomWhalen-MadMonsterParty.jpg",
+                          series: "",
+                          purchase_year: "2014",
+                          purchase_price: "$50.00",
+                          user_id: 1
+                        )
+
+# User Two
+print_thirteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Olly Moss", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Howl's Moving Castle, Variant",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_OllyMoss-HowlsMovingCastle.jpeg",
+                          series: "Howl's Moving Castle",
+                          purchase_year: "2013",
+                          purchase_price: "$75.00",
+                          user_id: 2
+                        )
+
+print_fourteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Kilian Eng", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The Terminator",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_KilianEng-TheTerminator.jpeg",
+                          series: "The Terminator",
+                          purchase_year: "2013",
+                          purchase_price: "$50.00",
+                          user_id: 2
+                        )
+
+print_fifthteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Ken Taylor", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The Dark Word",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_KenTaylor-Thor.jpeg",
+                          series: "Thor",
+                          purchase_year: "2013",
+                          purchase_price: "$50.00",
+                          user_id: 2
+                        )
+
+print_sixteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Kevin Tong", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "The World's End",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_KevinTong-TheWorldsEnd.jpeg",
+                          series: "The World's End",
+                          purchase_year: "2013",
+                          purchase_price: "$50.00",
+                          user_id: 2
+                        )
+
+print_seventeen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Olly Moss", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Spirited Away",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_OllyMoss-SpritedAway.jpeg",
+                          series: "Spirited Away",
+                          purchase_year: "2013",
+                          purchase_price: "$80.00",
+                          user_id: 2
+                        )
+
+print_eightteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Tyler Stout", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Django Unchained",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_TylerStout-DjanoUnchained.jpeg",
+                          series: "Django Unchained",
+                          purchase_year: "2013",
+                          purchase_price: "$50.00",
+                          user_id: 2
+                        )
+
+print_nineteen = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Ken Taylor", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "An Unexpected Journey",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_KenTaylor-TheHobbit.jpg",
+                          series: "The Hobbit",
+                          purchase_year: "2014",
+                          purchase_price: "$70.00",
+                          user_id: 2
+                        )
+
+print_twenty = Print.create(
+                          category: "Screen Print",
+                          creator_individual: "Martin Ansin", 
+                          creator_company: "Mondo",
+                          creator_printer: "D&L"
+                          title: "Ghost in the Shell, Foil Variant",
+                          photo: "https://dl.dropboxusercontent.com/u/9772835/Mondo%27s%20Posters/Mondo_MartinAnsin-GhostInTheShell.jpg",
+                          series: "Ghost in the Shell",
+                          purchase_year: "2014",
+                          purchase_price: "$90.00",
+                          user_id: 2
+                        )
