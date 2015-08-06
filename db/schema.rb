f1c29b918dfdac21108a731418d269d23f3ac55a
Silvia Brown <siltastic@gmail.com>
2015-08-06 14:04:33 -0400

    Added detailed user information to the database.

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150805190647) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "prints_id"
+    t.integer  "print_id"
     t.integer  "user_id"
     t.string   "content"
     t.string   "username"
@@ -30,16 +30,22 @@ ActiveRecord::Schema.define(version: 20150805190647) do
     t.string   "category"
     t.string   "creator_individual"
     t.string   "creator_company"
+    t.string   "creator_printer"
     t.string   "title"
     t.string   "photo"
     t.string   "series"
-    t.string   "purchase_date"
+    t.string   "purchase_year"
     t.string   "purchase_price"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "website"
+    t.string   "twitter"
+    t.string   "bio"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
